1ef8a990b201fbf1539a3f7ea076ff343de1290b
Adel Shehadeh <adelshehadeh@Adels-MBP.home>
2017-09-17 16:49:52 +0200

    configured apiserver to run with flask on boot

---
+++ myproject/apiserver.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from goose import Goose
+app = Flask(__name__)
+
+@app.route("/api")
+def articleExtractor():
+    
+    url = request.args.get('url','')
+    
+    articleObject = []
+    
+    print("Program started ...")
+    
+    articleExtractor = Goose()
+    article = articleExtractor.extract(url=url)
+    
+    #build article content
+    articleBody = ""
+    for letter in article.cleaned_text:
+        articleBody+=str(letter.encode('utf-8', 'ignore'))
+    
+
+    articleObject.append(article.title)
+    articleObject.append(article.meta_description)
+    articleObject.append(articleBody)
+    
+    #return article main text
+    return jsonify(articleObject)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
