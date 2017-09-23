ec8918dff2392394892e09127366959f3d2c992f
Adel Shehadeh <adelshehadeh@Adels-MBP.home>
2017-09-23 22:48:58 +0200

    switched to newsletter library to permanently solve goose's recursion limit error reached

--- apiserver/apiserver.py
+++ apiserver/apiserver.py
@@ -1,31 +1,27 @@
 from flask import Flask, request, jsonify
 from goose import Goose
+from newspaper import Article
+
+
 app = Flask(__name__)
 app.debug = True
 
-@app.route("/api")
+
+@app.route('/api')
+
 def articleExtractor():
-    
+    articleObject = []
     url = request.args.get('url','')
     
-    articleObject = []
+    article = Article(url)
+    article.download()
+    article.parse()
     
-    print("Program started ...")
     
-    articleExtractor = Goose()
-    article = articleExtractor.extract(url=url)
-    
-    #build article content
-    articleBody = ""
-    for letter in article.cleaned_text:
-        articleBody+=str(letter.encode('utf-8', 'ignore'))
-    
-
     articleObject.append(article.title)
-    articleObject.append(article.meta_description)
-    articleObject.append(articleBody)
+    articleObject.append(article.text[:])
     
-    #return article main text
-    return jsonify(articleObject)
+    return jsonify(aritcleText=articleObject[1], aritcleTitle=articleObject[0])
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
