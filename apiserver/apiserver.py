bac71054b7d5c9e84f3c2689fda5b43f43501b83
Adel Shehadeh <adelshehadeh@Adels-MacBook-Pro.local>
2017-10-01 17:54:26 +0200

    added a phrases blacklist to filter out unwanted content

--- apiserver/apiserver.py
+++ apiserver/apiserver.py
@@ -12,6 +12,8 @@ app.debug = True
 def articleExtractor():
     articleObject = []
     url = request.args.get('url','')
+    phrasesBlackList = ["Image copyright AFP", "Image copyright Reuters Image caption", "Getty Images Image caption", "AFP Image caption","Image caption"]
+
     
     article = Article(url)
     article.download()
@@ -20,12 +22,25 @@ def articleExtractor():
     
     
     articleObject.append(article.title)
-    articleObject.append(article.text[:])
+    
+    #clean up article body from unwanted phrase, then add it
+    rawArticleText = article.text[:]
+    for blackListedPhrase in phrasesBlackList:
+        rawArticleText = rawArticleText.replace(blackListedPhrase,'')
+    articleObject.append(rawArticleText)
+
     articleObject.append(article.authors)
     articleObject.append(article.top_image)
-    articleObject.append(article.summary)
+    
+    #clean up summary from unwanted phrase, then add it
+    rawSummaryText = article.summary
+    for blackListedPhrase in phrasesBlackList:
+        rawSummaryText = rawSummaryText.replace(blackListedPhrase,'')
+    articleObject.append(rawSummaryText)
+    
     
     return jsonify(articleText=articleObject[1], articleTitle=articleObject[0], articleAuthors=articleObject[2], articleTopImage=articleObject[3], articleNLPSummary=articleObject[4])
+    #return jsonify(articleObject)
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
